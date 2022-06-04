809f24cdda6d9f1f1de24b1e11a43fb7984c63f4
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2022-06-04 20:20:14 +0430

    remove unused imports

--- src/public/components/loading/Loading.tsx
+++ src/public/components/loading/Loading.tsx
@@ -1,7 +1,6 @@
-import { networkContext } from "private/features/network/context/networkContext";
 import { useLoading } from "public/hooks";
 import React from "react";
-import { FC, useContext, useEffect, useState } from "react";
+import { FC } from "react";
 import { LoadingPropsI } from "./Loading.type";
 
 export const Loading: FC<LoadingPropsI> = props => {
