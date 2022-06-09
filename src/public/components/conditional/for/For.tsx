8ab847599b1c9644e982fa365242b0ade102078b
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2022-06-09 19:41:49 +0430

    add more options

--- src/public/components/conditional/for/For.tsx
+++ src/public/components/conditional/for/For.tsx
@@ -1,16 +1,23 @@
 import React, { Fragment, useMemo } from "react";
 import { ReactElement } from "react";
 
+type renderFunc<T> = (item: T, index?: number) => ReactElement | ReactElement[]
+type renderFuncWithInfo<T> = (item: T, index?: number, isFirst?: boolean, isLast?: boolean) => ReactElement | ReactElement[]
+
 export const For = <T,>(props: {
     list: T[],
-    children: (item: T, index?: number, isFirst?: boolean, isLast?: boolean) => ReactElement | ReactElement[],
+    children: renderFuncWithInfo<T>,
+    firstItem?: renderFunc<T>,
+    lastItem?: renderFunc<T>,
 }): ReactElement | null => {
 
-    const lastIndex = useMemo(() => props.list.length - 1, [props.list])
+    const listLength: number = useMemo(() => props.list.length, [props.list])
 
     return (
         <Fragment>
-            {props.list.map((item, index) => props.children(item, index, index === 0, index === lastIndex))}
+            {props.list.slice(0, 1).map((item, index) => props.firstItem ? props.firstItem(item, index) : props.children(item, index, true, false))}
+            {props.list.slice(1, listLength - 1).map((item, index) => props.children(item, index, false, false))}
+            {props.list.slice(listLength - 1, listLength).map((item, index) => props.firstItem ? props.firstItem(item, index) : props.children(item, index, false, true))}
         </Fragment>
     )
 }
